b4ae14bc4692e8b86a63f939967ddcc3db2ee6b0
Bas Suckling <suckling.w.s@gmail.com>
2019-10-26 14:55:39 +1300

    initialised index.js and server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const server = express()
+
+server.engine('hbs', hbs({
+    defaultLayout: 'main',
+    extname: 'hbs'
+  }))
+
+server.set('view engine', 'hbs') // allow access to hbs file directory
+server.use(express.static('public')) // allow access to public folder
+server.use(express.urlencoded({extended: false})) //need clarification about this
+
+server.get('/', (req, res) {
+    res.send('<h1>Encrypto!</h1>')
+
+})
