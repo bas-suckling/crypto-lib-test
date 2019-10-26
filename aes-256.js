7080dd9d46277fe80cfb8a61b14dac2ed19248b3
Bas Suckling <suckling.w.s@gmail.com>
2019-10-26 16:46:13 +1300

    reformatting using handlebars

---
+++ aes-256.js
@@ -0,0 +1,43 @@
+const crypto = require('crypto');
+
+
+let key = '12345678123456781234567812345678'
+let iv = '1234567812345678'
+
+//initialisation vector and key are hard coded, but can be generated using the functions below
+
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return { secretMessage: text, iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+}
+
+function decrypt(text) {
+    let iv = Buffer.from(text.iv, 'hex');
+    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(encryptedText);
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+function generateKey() {
+    return key = crypto.randomBytes(32);
+}
+
+function generateIV() {
+    return iv = crypto.randomBytes(16)
+}
+
+console.log(encrypt("test encryption"))
+
+
+module.exports = {
+    encrypt: encrypt,
+    decrypt: decrypt,
+    generateKey: generateKey,
+    generateIV: generateIV
+}
+
+
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-
 const server = express()
+const crypto = require('crypto')
 
 server.engine('hbs', hbs({
     defaultLayout: 'main',
@@ -13,11 +13,12 @@ server.use(express.static('public')) // allow access to public folder
 server.use(express.urlencoded({extended: false})) //need clarification about this
 
 server.get('/', (req, res) => {
-    res.send('<h1>Encrypto!</h1>')
+    const home = './home.hbs'
+    res.render(home)
 })
 
-server.get('/aes-128', (req, res) => {
-    res.sendFile(__dirname + '/pages/AES-128.html')
+server.get('/aes-256', (req, res) => {
+    res.sendFile(__dirname + '/pages/AES-256.html')
 })
 
 module.exports = server;
