669b11d485083e2242a23ce3ed0720a5c5fe85a8
Bas Suckling <suckling.w.s@gmail.com>
2019-10-26 15:09:22 +1300

    issues with server

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express')
-const hbs = require('express-handlebars')
+// const hbs = require('express-handlebars')
 
 const server = express()
 
-server.engine('hbs', hbs({
-    defaultLayout: 'main',
-    extname: 'hbs'
-  }))
+// server.engine('hbs', hbs({
+//     defaultLayout: 'main',
+//     extname: 'hbs'
+//   }))
 
-server.set('view engine', 'hbs') // allow access to hbs file directory
+// server.set('view engine', 'hbs') // allow access to hbs file directory
 server.use(express.static('public')) // allow access to public folder
 server.use(express.urlencoded({extended: false})) //need clarification about this
 
-server.get('/', (req, res) {
-    res.send('<h1>Encrypto!</h1>')
-
+server.get('/', (req, res) => {
+        res.send('<h1>Encrypto!</h1>')
 })
